6e3156e8d370c0346b7f48160e78c9c36f1b6e08
luanlzsn <331848894@qq.com>
2017-04-29 14:50:53 +0800

    Payment

--- Traber/Classes/Payment/Controller/PaymentVC.swift
+++ Traber/Classes/Payment/Controller/PaymentVC.swift
@@ -8,12 +8,65 @@
 
 import UIKit
 
-class PaymentVC: AntController {
+class PaymentVC: AntController,UITableViewDelegate,UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    let sectionTitleArray = ["Payment Methods","Promotions"]
+    let paymentMethods = ["****5768"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.separatorInset = UIEdgeInsets.zero
+        tableView.layoutMargins = UIEdgeInsets.zero
+    }
+    
+    // MARK: UITableViewDelegate,UITableViewDataSource
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sectionTitleArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return section == 0 ? paymentMethods.count + 1 : 1
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 55
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 0.01
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header = UIView(frame: CGRect(x: 0, y: 0, width: kScreenWidth, height: 55))
+        let sectionLabel = UILabel(frame: CGRect(x: 15, y: 30, width: kScreenWidth - 30, height: 25))
+        sectionLabel.font = UIFont.systemFont(ofSize: 12)
+        sectionLabel.textColor = UIColor.init(rgb: 0x6d6d72)
+        sectionLabel.text = sectionTitleArray[section]
+        header.addSubview(sectionLabel)
+        return header
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
+        if indexPath.section == 0 {
+            if indexPath.row < paymentMethods.count {
+                cell.textLabel?.text = paymentMethods[indexPath.row]
+                cell.imageView?.image = UIImage(named: "visa_icon")
+            } else {
+                cell.textLabel?.text = "Add Payment Method"
+                cell.imageView?.image = nil
+            }
+        } else {
+            cell.textLabel?.text = "Add Promo/Gift Code"
+            cell.imageView?.image = nil
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 
     override func didReceiveMemoryWarning() {
